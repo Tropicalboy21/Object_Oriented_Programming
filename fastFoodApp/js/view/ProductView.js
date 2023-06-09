5ecdfc629fdd785786002bc0aea009bddd559670
Lenin Ugalde <93264177+Tropicalboy21@users.noreply.github.com>
2023-06-08 18:55:43 -0600

    Update ProductView.js

--- fastFoodApp/js/view/ProductView.js
+++ fastFoodApp/js/view/ProductView.js
@@ -1,12 +1,14 @@
 export class ProductView {
-    constructor(parent, product, callback) {
+    constructor(parent, product, callback, callback2) {
 
         this.parent = parent;
         this.product = product;
         this.callback = callback;
+        this.callback2 = callback2;
 
         var container = document.createElement('div');
         container.className = 'product_container';
+        container.id = "productContainer";
         this.parent.appendChild(container);
 
         var title = document.createElement('p');
@@ -24,14 +26,29 @@ export class ProductView {
         image.src = this.product.image;
         container.appendChild(image);
 
+        var buttonsContainer = document.createElement('div');
+        container.appendChild(buttonsContainer);
+        buttonsContainer.className = "buttons_container";
+
         var addButton = document.createElement('div');
         addButton.className = 'product_add_button';
         addButton.innerHTML = 'ADD';
-        container.appendChild(addButton);
+        buttonsContainer.appendChild(addButton);
         addButton.onclick = this.onAddButton.bind(this);
+
+        var removebutton = document.createElement('div');
+        removebutton.className = 'product_remove_button';
+        removebutton.innerHTML = 'REMOVE';
+        buttonsContainer.appendChild(removebutton);
+        removebutton.onclick = this.onRemoveButton.bind(this);
     }
 
     onAddButton(event) {
         this.callback(this.product);
     }
+
+    onRemoveButton(event) {
+        this.callback2(this.product);
+    }
+
 }
